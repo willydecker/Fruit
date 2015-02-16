b215a02a2a13c406a351b0338945b01aa8fe4a26
willydecker <wdw1062@saintjoe.edu>
2015-02-16 12:13:27 -0500

    fruit

---
+++ fruit.js
@@ -0,0 +1,4 @@
+var fruit_file = "[/n " + [
+	'{"Fruit" : "orange", "color" : "orange, "size" : "small"}'
+	'{"Fruit" : "apple", "color" : "red", "size" : "small"}'
+	'{"Fruit" : "watermelon", "color" : "green", "size" : "large"}'
